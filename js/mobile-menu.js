94df4189d14e7598c14876fc6a342a93990e1d29
Mariia <1907mss@gmail.com>
2019-08-03 23:55:51 +0300

    slider is added, styles are modified

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,12 @@
-  var navMain = document.querySelector('.main-nav');
+  var navMain = document.querySelector('.main-nav__wrapper');
   var navToggle = document.querySelector('.main-nav__toggle');
 
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--opened')) {
-      navMain.classList.remove('main-nav--opened');
-      navMain.classList.add('main-nav--closed');
+    if (navMain.classList.contains('opened')) {
+      navMain.classList.remove('opened');
     } else {
-      navMain.classList.add('main-nav--opened');
-      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('opened');
     }
   });
--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,8 @@
 var linkOpen = document.querySelectorAll(".add-to-cart");
 var linkClose = document.querySelector(".modal__button");
 var popup = document.querySelector(".modal-order");
+var body = document.querySelector("body");
+var overlay = document.querySelector(".overlay");
 var isStorageSupport = true;
 var storage="";
 
@@ -14,16 +16,24 @@ if (linkOpen && popup && linkClose) {
     linkOpen[i].addEventListener("click", function (event) {
       event.preventDefault();
       popup.classList.add("modal-show");
+      body.style.overflow = "hidden";
+      overlay.classList.add("active");
     });
   }
-  linkClose.addEventListener("click", function (event) {
-    event.preventDefault();
-    popup.classList.remove("modal-show");
-  });
+    window.addEventListener("click", function (evt) {
+        if(evt.target.classList.contains("modal__button") || evt.target.classList.contains("overlay")){
+            event.preventDefault();
+            popup.classList.remove("modal-show");
+            body.style.overflow = "auto";
+            overlay.classList.remove("active");
+        }
+    });
   window.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
       if (popup.classList.contains("modal-show")) {
         popup.classList.remove("modal-show");
+        body.style.overflow = "auto";
+        overlay.classList.remove("active");
       }
     }
   });
