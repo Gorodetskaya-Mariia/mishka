b12ac703c896022047c0a1b223aeb2a5e5d10ab6
Mariia <1907mss@gmail.com>
2019-09-14 22:25:44 +0300

    multi-lang is added

---
+++ js/main-page-translations.js
@@ -0,0 +1,211 @@
+var linguJSON = {
+  "languages": [
+      {
+          "lang_name": "Русский",
+          "lang_code": "rus",
+          "url_pattern": "?"
+      },
+      {
+        "lang_name": "English",
+        "lang_code": "en",
+        "url_pattern": "?"
+    }
+  ],
+  "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a></div>",
+    "custom_lang_switcher_container_id": "langlinks",
+  "translated_segments": [
+      {
+          "source": "Открыть меню",
+          "target": "Open menu"
+      },
+      {
+          "source": "Каталог товаров",
+          "target": "Catalog"
+      },
+      {
+          "source": "Вязание на заказ",
+          "target": "Custom knitting"
+      },
+      {
+          "source": "Поиск по сайту",
+          "target": "Search"
+      },
+      {
+        "source": "Корзина: пока пуста",
+        "target": "Cart: 0"
+      },
+      {
+        "source": "Новые поступления",
+        "target": "New Releases"
+      },
+      {
+        "source": "Распродажа",
+        "target": "Sell-out"
+      },
+      {
+        "source": "Бесплатная доставка по России",
+        "target": "Free delivery in Russia"
+      },
+      {
+        "source": "Милые штуки ручной работы для дома",
+        "target": "Cute, handmade stuff for house"
+      },
+      {
+        "source": "Категории товаров",
+        "target": "Category"
+      },
+      {
+        "source": "Предметы интерьера",
+        "target": "Interior Items"
+      },
+      {
+        "source": "Детские игрушки",
+        "target": "Toys, Gifts"
+      },
+      {
+        "source": "Вязаные корзинки",
+        "target": "Knitted baskets"
+      },
+      {
+        "source": "Товар недели",
+        "target": "Product of the week"
+      },
+      {
+        "source": "Экологически чистая пряжа, ручная работа. Доступны в шести расцветках, и трех размерах.",
+        "target": "Environmentally friendly yarn, handmade. Available in six colors and three sizes."
+      },
+      {
+        "source": "Цвет:",
+        "target": "Color:"
+      },
+      {
+        "source": "«Тиффани»/серый",
+        "target": "«Tiffany»/ grey"
+      },
+      {
+        "source": "Диаметр:",
+        "target": "Diameter:"
+      },
+      {
+        "source": "25 см",
+        "target": "25 cm"
+      },
+      {
+        "source": "15 см",
+        "target": "15 cm"
+      },
+      {
+        "source": "Высота:",
+        "target": "Height:"
+      },
+      {
+        "source": "Цена: 990 руб.",
+        "target": "Price: 990 RUB"
+      },
+      {
+        "source": "Заказать",
+        "target": "Order"
+      },
+      {
+        "source": "Коротко о нас:",
+        "target": "About us:"
+      },
+      {
+        "source": "Экологически чистые материалы",
+        "target": "Environmentally friendly materials"
+      },
+      {
+        "source": "Скандинавский стиль по российской цене",
+        "target": "Scandinavian style at the Russian price"
+      },
+      {
+        "source": "Увеличивает лайки на фотографиях",
+        "target": "Increases likes of photos"
+      },
+      {
+        "source": "Связано вручную с любовью и умилением",
+        "target": "Hand-made with love"
+      },
+      {
+        "source": "Поддержка отечественного производителя",
+        "target": "Support for domestic manufacturer"
+      },
+      {
+        "source": "Поставляется в подарочной упаковке",
+        "target": "Shipping in a gift box"
+      },
+      {
+        "source": "Отзывы",
+        "target": "Reviews"
+      },
+      {
+        "source": "Я молодая мама, но все равно фотографии не набирали и близко такого количества лайков, как у популярных инстамамочек. В отчаянии, я накупила аксессуаров и игрушек в Мишке, и мои фотографии сразу стали более стильными, а также набирают больше лайков!",
+        "target": "I’m a young mother, and my photos did not get so many likes as popular instamothers. But, when I have bought accessories and toys in Mishka, my photos immediately became more stylish and also gain more likes!"
+      },
+      {
+        "source": "Анастасия Красильникова",
+        "target": "Anastasya Matveeva"
+      },
+      {
+        "source": "Предыдущий отзыв",
+        "target": "Previous review"
+      },
+      {
+        "source": "Следующий отзыв",
+        "target": "Next review"
+      },
+      {
+        "source": "Написать",
+        "target": "Write"
+      },
+      {
+        "source": "Контакты",
+        "target": "Contacts"
+      },
+      {
+        "source": "адрес:",
+        "target": "address:"
+      },
+      {
+        "source": "г. Санкт-Петербург, ул. Большая Конюшенная, д. 19/8, офис 101",
+        "target": "St. Petersburg, Nevskaya street, 19/8, office 101"
+      },
+      {
+        "source": "Как нас найти",
+        "target": "Map"
+      },
+      {
+        "source": "Напишите нам",
+        "target": "Write us"
+      },
+      {
+        "source": "Давайте дружить!",
+        "target": "Became friends!"
+      },
+      {
+        "source": "Мы в Инстаграме",
+        "target": "Instagram"
+      },
+      {
+        "source": "Мы в Фейсбуке",
+        "target": "Facebook"
+      },
+      {
+        "source": "Мы в Твиттере",
+        "target": "Twitter"
+      },
+      {
+        "source": "Разработано",
+        "target": "Website by"
+      },
+      {
+        "source": "Добавить в корзину",
+        "target": "Add into a cart"
+      },
+      {
+        "source": "Выберите размер:",
+        "target": "Choose size:"
+      },
+  ]
+};
+
